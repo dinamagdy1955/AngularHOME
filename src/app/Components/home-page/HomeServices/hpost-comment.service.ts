8a6d67958a2af5eee245c4f6d88c9e80804c4ee0
dinamagdy1955 <dinamagdy.1995@gmail.com>
2021-05-22 21:29:36 +0200

    edit

--- src/app/Components/home-page/HomeServices/hpost-comment.service.ts
+++ src/app/Components/home-page/HomeServices/hpost-comment.service.ts
@@ -1,63 +1,119 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { IHomeComment } from '../ViewModel/ihome-comment'
+import { IHomeComment } from '../ViewModel/ihome-comment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HPostCommentService {
-  postCounts: BehaviorSubject<number>
-  constructor( private db: AngularFirestore) {
-    this.postCounts = new BehaviorSubject<number>(4)
-   }
+  postCounts: BehaviorSubject<number>;
+  constructor(private db: AngularFirestore) {
+    this.postCounts = new BehaviorSubject<number>(4);
+  }
 
-   writeComment(comment:IHomeComment, postId,AUTHId) {
+  writeComment(comment: IHomeComment, postId, AUTHId) {
     let uid = localStorage.getItem('uid');
     return new Promise<any>((res, rej) => {
-      this.db.collection('users-details').doc(uid)
-      .collection('MyFriendsPosts').doc(postId).collection('postsComments').add(comment)
-      .then((res) => {
-        console.log(res.id)
-        this.db.collection('users-details').doc(AUTHId)
-        .collection('MyPosts').doc(postId).collection('postsComments').doc(res.id).set(comment)
-       }, (err) => rej(err))
+      if (uid == AUTHId) {
+        this.db
+          .collection('users-details')
+          .doc(uid)
+          .collection('MyPosts')
+          .doc(postId)
+          .collection('postsComments')
+          .add(comment)
+          .then(
+            (res) => {
+              console.log(res.id);
+              this.db
+                .collection('users-details')
+                .doc(AUTHId)
+                .collection('MyFriendsPosts')
+                .doc(postId)
+                .collection('postsComments')
+                .doc(res.id)
+                .set(comment);
+            },
+            (err) => rej(err)
+          );
+      } else {
+        this.db
+          .collection('users-details')
+          .doc(uid)
+          .collection('MyFriendsPosts')
+          .doc(postId)
+          .collection('postsComments')
+          .add(comment)
+          .then(
+            (res) => {
+              console.log(res.id);
+              this.db
+                .collection('users-details')
+                .doc(AUTHId)
+                .collection('MyPosts')
+                .doc(postId)
+                .collection('postsComments')
+                .doc(res.id)
+                .set(comment);
+            },
+            (err) => rej(err)
+          );
+      }
     });
   }
 
   getPostComments2(postId, param?) {
     let uid = localStorage.getItem('uid');
     if (param != undefined) {
-      return  this.db.collection('users-details').doc(uid)
-      .collection('MyPosts').doc(postId).collection('postsComments', ref => ref.orderBy('CommentDate', 'desc').limit(5)
-      .startAfter(param)).snapshotChanges();
-    
+      return this.db
+        .collection('users-details')
+        .doc(uid)
+        .collection('MyPosts')
+        .doc(postId)
+        .collection('postsComments', (ref) =>
+          ref.orderBy('CommentDate', 'desc').limit(5).startAfter(param)
+        )
+        .snapshotChanges();
     } else {
-      return   this.db.collection('users-details').doc(uid)
-      .collection('MyPosts').doc(postId).collection('postsComments', ref => ref.orderBy('CommentDate', 'desc').limit(5)).snapshotChanges();
-     
-    
-      
+      return this.db
+        .collection('users-details')
+        .doc(uid)
+        .collection('MyPosts')
+        .doc(postId)
+        .collection('postsComments', (ref) =>
+          ref.orderBy('CommentDate', 'desc').limit(5)
+        )
+        .snapshotChanges();
     }
   }
 
   getFriendPostComments(postId, param?) {
     let uid = localStorage.getItem('uid');
     if (param != undefined) {
-     return   this.db.collection('users-details').doc(uid)
-      .collection('MyFriendsPosts').doc(postId).collection('postsComments', ref => ref.orderBy('CommentDate', 'desc').limit(5).startAfter(param)).snapshotChanges();
-     
+      return this.db
+        .collection('users-details')
+        .doc(uid)
+        .collection('MyFriendsPosts')
+        .doc(postId)
+        .collection('postsComments', (ref) =>
+          ref.orderBy('CommentDate', 'desc').limit(5).startAfter(param)
+        )
+        .snapshotChanges();
     } else {
-     
-      return this.db.collection('users-details').doc(uid)
-      .collection('MyFriendsPosts').doc(postId).collection('postsComments', ref => ref.orderBy('CommentDate', 'desc').limit(5)).snapshotChanges();
-     
-       
+      return this.db
+        .collection('users-details')
+        .doc(uid)
+        .collection('MyFriendsPosts')
+        .doc(postId)
+        .collection('postsComments', (ref) =>
+          ref.orderBy('CommentDate', 'desc').limit(5)
+        )
+        .snapshotChanges();
     }
   }
 
   getMore() {
-    this.postCounts.next(this.postCounts.value + 4)
+    this.postCounts.next(this.postCounts.value + 4);
   }
-
 }
--- src/app/Components/home-page/home-post-comment/home-post-comment.component.ts
+++ src/app/Components/home-page/home-post-comment/home-post-comment.component.ts
@@ -5,47 +5,68 @@ import { HPostCommentService } from '../HomeServices/hpost-comment.service';
 @Component({
   selector: 'app-home-post-comment',
   templateUrl: './home-post-comment.component.html',
-  styleUrls: ['./home-post-comment.component.scss']
+  styleUrls: ['./home-post-comment.component.scss'],
 })
 export class HomePostCommentComponent implements OnInit {
-  @Input() PostID: string
-  @Input() admins
-  @Input() AUTHId: string
-  subscriptions: Subscription[] = []
-  getComments = []
-  
-  userID
+  @Input() PostID: string;
+  @Input() admins;
+  @Input() AUTHId: string;
+  subscriptions: Subscription[] = [];
+  getComments = [];
 
-  turnOff: boolean = false
-  counter: number = 0
-  constructor( public commentService:HPostCommentService) { }
+  userID;
+
+  turnOff: boolean = false;
+  counter: number = 0;
+  constructor(public commentService: HPostCommentService) {}
   identify(index, c) {
-    return c.id
+    return c.id;
   }
   ngOnInit(): void {
+    this.userID = localStorage.getItem('uid');
 
-    this.userID = localStorage.getItem('uid')
-    let sub = this.commentService.getPostComments2(this.PostID).subscribe(res => {
-      this.getComments = res.map(e => {
-        return {
-          id: e.payload.doc.id,
-          ...(e.payload.doc.data() as object),
-          doc: e.payload.doc
-        }
-      })
-      this.hideBtn(res);
-    })
-    this.subscriptions.push(sub)
-  }
-
-
-  hideBtn(res) {
-    if ((res.length % 5 != 0 && res.length - this.counter == 0) || res.length % 5 != 0 || res.length == 0) {
-      this.turnOff = true
+    if (this.AUTHId == this.userID) {
+      let sub = this.commentService
+        .getPostComments2(this.PostID)
+        .subscribe((res) => {
+          this.getComments = res.map((e) => {
+            return {
+              id: e.payload.doc.id,
+              ...(e.payload.doc.data() as object),
+              doc: e.payload.doc,
+            };
+          });
+          console.log('mu posts', this.getComments);
+          this.hideBtn(res);
+        });
+      this.subscriptions.push(sub);
     } else {
-      this.turnOff = false
+      let sub = this.commentService
+        .getFriendPostComments(this.PostID)
+        .subscribe((res) => {
+          this.getComments = res.map((e) => {
+            return {
+              id: e.payload.doc.id,
+              ...(e.payload.doc.data() as object),
+              doc: e.payload.doc,
+            };
+          });
+          console.log('friend posts', this.getComments);
+          this.hideBtn(res);
+        });
+      this.subscriptions.push(sub);
     }
   }
 
-
+  hideBtn(res) {
+    if (
+      (res.length % 5 != 0 && res.length - this.counter == 0) ||
+      res.length % 5 != 0 ||
+      res.length == 0
+    ) {
+      this.turnOff = true;
+    } else {
+      this.turnOff = false;
+    }
+  }
 }
